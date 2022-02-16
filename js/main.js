15d1bf1859cb7fedf3d755192715acfbfb153483
zishan344 <zishanahmed344@gmail.com>
2022-02-17 00:54:52 +0600

    done the balance section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
+function getInput(idInput) {
+  const inputValue = document.getElementById(idInput).value;
+  if (inputValue < 0) {
+    alert("don't writing a negative number");
+  } else {
+    return parseFloat(inputValue);
+  }
+}
 document
   .getElementById("calcalute-salary")
   .addEventListener("click", function () {
-    console.log("ha ha wellcome dear dedicated paerson");
+    // input field
+    const income = getInput("income");
+
+    const food = getInput("food");
+
+    const rent = getInput("rent");
+
+    const clothes = getInput("clothes");
+    // update total amount
+    const amount = food + rent + clothes;
+    if (amount > income || isNaN(income) || isNaN(food) || isNaN(clothes)) {
+      alert("you have note enough amount");
+    } else {
+      const expence = document.getElementById("expence");
+      expence.innerText = amount;
+      //update tota Balance
+      const totalBalane = document.getElementById("balance");
+      totalBalane.innerText = income - parseInt(expence.innerText);
+    }
   });
