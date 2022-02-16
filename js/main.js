b843199bfa1d7b60920bf6b4981b179f145b19ec
zishan344 <zishanahmed344@gmail.com>
2022-02-17 03:02:55 +0600

    done assignment

--- js/main.js
+++ js/main.js
@@ -19,13 +19,31 @@ document
     const clothes = getInput("clothes");
     // update total amount
     const amount = food + rent + clothes;
-    if (amount > income || isNaN(income) || isNaN(food) || isNaN(clothes)) {
+    if (amount > income) {
       alert("you have note enough amount");
     } else {
       const expence = document.getElementById("expence");
       expence.innerText = amount;
       //update tota Balance
-      const totalBalane = document.getElementById("balance");
-      totalBalane.innerText = income - parseInt(expence.innerText);
+      const totalBalance = document.getElementById("balance");
+      totalBalance.innerText = income - parseInt(expence.innerText);
     }
   });
+
+document.getElementById("saving").addEventListener("click", function () {
+  const balance = document.getElementById("balance");
+  const income = getInput("income");
+  const parsentInput = getInput("parsent-box");
+  const parsent = (income * parsentInput) / 100;
+  const savings = document.getElementById("saving-amount");
+  const remaining = document.getElementById("remaining-balance");
+  if (parsent > parseFloat(balance.innerText)) {
+    alert("you have not enough money for savings");
+  } else if (isNaN(parsent) || isNaN(remaining) || isNaN(savings)) {
+    alert("enter a valid input");
+  } else {
+    savings.innerText = parsent;
+    remaining.innerText =
+      parseFloat(balance.innerText) - parseFloat(savings.innerText);
+  }
+});
